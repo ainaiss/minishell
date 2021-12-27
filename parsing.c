b9418df05422c7deacc8a18c0e54692c01e94b87
Aissam Barchil <abarchil@e1r9p7.1337.ma>
2021-12-27 20:22:59 +0100

    90% parsing done | fixed all parsing bugs | set redirection variable

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: abarchil <abarchil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 06:07:57 by abarchil          #+#    #+#             */
-/*   Updated: 2021/12/27 19:06:03 by abarchil         ###   ########.fr       */
+/*   Updated: 2021/12/27 20:22:02 by abarchil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,52 +40,32 @@ void	parsing_word(char *command, t_words *cmds)
 	}
 }
 
-int		count_word(char *s)
-{
-	int		count;
-	int 	words;
-
-	count  = 0;
-	words = 0;
-	while (s[count])
-	{
-		if (s[count] && s[count] != SPACE)
-		{
-			words++;
-			while (s[count] && s[count] != SPACE)
-				count++;
-		}
-		while (s[count] && s[count] == SPACE)
-			count++;
-	}
-	return (words);
-}
-
 t_cmd	*parse_commands(t_words *words, t_cmd *cmd)
 {
-	int		count;
-	int		args_count;
-	int		tmp;
+	int	i;
 
-	tmp = 0;
-	count = 0;
-	args_count = 0;
+	i = -1;
 	cmd = (t_cmd *)malloc(sizeof(t_cmd));
 	if (!cmd)
 		return NULL;
-	cmd->args = (char **)malloc(sizeof(char *) * count_word(words->words) + 1);
-	while (args_count < count_word(words->words))
+	cmd->args =  ft_split(words->words, SPACE);
+	if (cmd->args[0][0] > 0)
+		cmd->command = cmd->args[0];
+	else
+		cmd->command = cmd->args[2];
+	while (cmd->args[++i])
 	{
-		while (words->words[count] && words->words[count] != SPACE)
-			count++;
-		while (words->words[count] && words->words[count] == SPACE)
-			count++;
-		cmd->args[args_count] = remchar(ft_substr(words->words, tmp, count - tmp), SPACE);
-		puts(cmd->args[args_count]);
-		args_count++;
-		tmp = count;
+		if (cmd->args[i][0] == REDIRECTION_IN)
+			cmd->red_pipe = 2;
+		else if (cmd->args[i][0] == REDIRECTION_OUT)
+			cmd->red_pipe = 3;
+		else if (cmd->args[i][0] == REDIRECTION_OUT_APPEND)
+			cmd->red_pipe = 4;
+		else if (cmd->args[i][0] == HER_DOC)
+			cmd->red_pipe = 5;
+		else if (cmd->next)
+			cmd->red_pipe = 1;
 	}
-	cmd->args[args_count] = NULL;
 	return (cmd);
 }
 
@@ -99,20 +79,6 @@ void	parsing(t_words *words, t_cmd *cmd)
 	ft_lstadd_back_cmd(cmd, parse_commands(words, cmd));
 }
 
-// void	ft_check_file(t_cmd *cmd, t_files *files)
+// void	ft_check_file(t_cmd *cmd, t_files *file)
 // {
-	
-// }
-// void	put_parse(t_cmd *cmd)
-// {
-// 	// int i = -1;
-// 	puts("======= command =======");
-// 	puts(cmd->command);
-// 	puts("======= argument =======");
-// 	if (cmd->args)
-// 		puts(cmd->args[0]);
-// 	// {
-// 	// 	while (cmd->args[++i])
-// 	// 		puts(cmd->args[i]);
-// 	// }
 // }
